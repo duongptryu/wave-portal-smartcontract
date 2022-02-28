dcc820d848e8ae107fe1e37bf184b8e72194f6b9
duongptryu <duongptryu@gmail.com>
2022-02-28 12:34:24 +0700

    Update event in solidity, store data in smart contract and randomess

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,15 +4,67 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal{
-    uint256 totalWaves;
 
-    constructor () {
-        console.log("Yo yo, My name is Duong, and I am write this smart contract");
+    uint256 private seed;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    function wave() public {
+    mapping(address => uint256) public lastWaveAt;
+
+    uint256 totalWaves;
+    Wave[] waves;
+
+    constructor () payable {
+        console.log("Yo yo, My name is Duong, and I am write this smart contract");
+        //initial seed
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        require(lastWaveAt[msg.sender] + 30 seconds < block.timestamp, "Wait 15m");
+        
+        //update the current time user wave
+        lastWaveAt[msg.sender] = block.timestamp;
+
         totalWaves ++;
         console.log("%s has waved", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has."
+        );
+
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,32 @@
 const hre = require("hardhat");
 
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
-    console.log("Contract deployed to: ", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+    console.log("Contract addy:", waveContract.address);
+  
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
+    const waveTxn = await waveContract.wave("This is wave #1");
     await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-
-    //add random person
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-    await waveContract.getTotalWaves();
+  
+    // const waveTxn2 = await waveContract.wave("This is wave #2");
+    // await waveTxn2.wait();
+  
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
 }
 
 const runMain = async () => {
